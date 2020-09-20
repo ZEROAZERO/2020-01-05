e797f03cf713a075ca0c9ad5a981818a942138b8
ZEROAZERO <249921551@qq.com>
2020-09-20 22:25:18 +0800

    update the content

--- leetcode/src/dynamic/Q638_ShoppingOffers.java
+++ leetcode/src/dynamic/Q638_ShoppingOffers.java
@@ -43,37 +43,65 @@ import java.util.Map;
 public class Q638_ShoppingOffers {
 
     private int shoppingOffers(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
-        Map<List<Integer>, Integer> map = new HashMap<>();
-        return shopping(price, special, needs, map);
+        return shopping(price, special, needs);
     }
 
-    private int shopping(List<Integer> price, List<List<Integer>> special, List<Integer> needs, Map<List<Integer>, Integer> map) {
-        if (map.containsKey(needs)) {
-            return map.get(needs);
-        }
-        int j = 0, res = dot(needs, price);
+    private int shopping(List<Integer> price, List<List<Integer>> special, List<Integer> needs) {
+        int res = dot(price, needs);
+        int i = 0;
         for (List<Integer> s: special) {
-            ArrayList<Integer> clone = new ArrayList<>(needs);
-            for (j=0; j<needs.size(); j++) {
-                int diff = needs.get(j) - s.get(j);
+            List<Integer> clone = new ArrayList<>(needs);
+            // 每种套餐 对于 A B。。。类月饼的需求
+            for (i=0; i<needs.size(); i++) {
+                int diff = clone.get(i) - s.get(i);
+                // 确保每个分量都不会超出范围
                 if (diff < 0) {
                     break;
                 }
-                clone.set(j, diff);
+                clone.set(i, diff);
             }
-            if (j == needs.size()) {
-                res = Math.max(res, s.get(j)+shopping(price, special, clone, map));
+            if (i == needs.size()) {
+                res = Math.min(res, s.get(i)+shopping(price, special, clone));
             }
         }
-        map.put(needs, res);
         return res;
     }
 
-    private int dot(List < Integer > a, List < Integer > b) {
-        int sum = 0;
-        for (int i = 0; i < a.size(); i++) {
-            sum += a.get(i) * b.get(i);
+    private int dot(List<Integer> price, List<Integer> needs) {
+        int ans = 0;
+        for (int i=0; i<price.size(); i++) {
+            ans += price.get(i) * needs.get(i);
         }
-        return sum;
+        return ans;
     }
+
+    public static void main(String[] args) {
+        // [2,5], [[3,0,5],[1,2,10]], [3,2]
+        List<Integer> price = new ArrayList<>();
+        price.add(2);
+        price.add(5);
+
+        List<List<Integer>> special = new ArrayList<>();
+        List<Integer> l1 = new ArrayList<>();
+        l1.add(3);
+        l1.add(0);
+        l1.add(5);
+        special.add(l1);
+        List<Integer> l2 = new ArrayList<>();
+        l2.add(1);
+        l2.add(2);
+        l2.add(10);
+        special.add(l2);
+
+        List<Integer> needs = new ArrayList<>();
+        needs.add(3);
+        needs.add(2);
+
+        Q638_ShoppingOffers q = new Q638_ShoppingOffers();
+        int ans = q.shoppingOffers(price, special, needs);
+        System.out.println(ans);
+    }
+
+
+
 }
